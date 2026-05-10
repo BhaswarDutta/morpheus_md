a3b948bdbcde4bcd2e795e41039e844c91097bf4
BhaswarDutta <mail2bhaswardutta@gmail.com>
2026-05-11 01:10:13 +0530

    Hook frontend code with FastAPI and make things work

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+
+# Serve frontend folder as static
+app.mount("/static", StaticFiles(directory="../frontend"), name="static")
 
 @app.get("/health")
 def check_health():
     return {"status": "ok"}
+
+@app.get("/")
+def home():
+    return FileResponse("../frontend/index.html")
