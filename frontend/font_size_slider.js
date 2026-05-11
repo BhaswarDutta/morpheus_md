daa7aae6b3f57e3edaa12901b514b924691ac13f
BhaswarDutta <mail2bhaswardutta@gmail.com>
2026-05-11 12:09:48 +0530

    Change default values of slider. Add double click to reset functionality

--- frontend/font_size_slider.js
+++ frontend/font_size_slider.js
@@ -10,3 +10,11 @@ fontSizeValue.innerHTML = `${fontSizeSlider.value}pt`;
 fontSizeSlider.oninput = function () {
   fontSizeValue.innerHTML = `${this.value}pt`;
 };
+
+/* DOUBLE CLICK TO RESET */
+
+fontSizeSlider.ondblclick = function () {
+  this.value = 8;
+
+  fontSizeValue.innerHTML = '8pt';
+};
